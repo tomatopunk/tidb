9ae0647856283beec12267e78b636f6b6923a5ee
tomatopunk <lion.zhang@mail.com>
2021-08-02 14:51:50 +0800

    distsql_select_result_test: migrate test-infra to testify

--- distsql/main_test.go
+++ distsql/main_test.go
@@ -2,9 +2,15 @@ package distsql
 
 import (
 	"github.com/pingcap/tidb/util/testbridge"
+	"go.uber.org/goleak"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
+	//testbridge.WorkaroundGoCheckFlags()
 	testbridge.WorkaroundGoCheckFlags()
+	opts := []goleak.Option{
+		goleak.IgnoreTopFunction("go.etcd.io/etcd/pkg/logutil.(*MergeLogger).outputLoop"),
+	}
+	goleak.VerifyTestMain(m, opts...)
 }
